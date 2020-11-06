b13b8ea2a44a8c3245fcbf3889ace27df3827613
BalthPa <balthazarpapin@gmail.com>
2020-11-06 16:45:51 +0100

    Add categorie controller and categorieform

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/categorie", name="categorie")
+     * @Route("/", name="categorie")
      */
-    public function index(): Response
+    public function index(Request $request) :Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $categories = $em->getRepository(Categorie::class)->findAll();
+
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->persist($categorie);
+            $em->flush();
+
+            $this->addFlash('success', 'Categorie ajoutée');
+        }
+
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categories' => $categories,
+            'ajoutCategorie' => $form->createView()
         ]);
     }
 }
