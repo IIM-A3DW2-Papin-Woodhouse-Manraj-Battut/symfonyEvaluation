c6689489512c684b9ecb750a4420475ff1f9b4ef
oscar woodhouse <oscarwoodhouse17@gmail.com>
2020-11-06 19:11:23 +0100

    form style v-6

--- src/Form/CoursType.php
+++ src/Form/CoursType.php
@@ -6,6 +6,7 @@ use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +18,7 @@ class CoursType extends AbstractType
             ->add('nom', TextType::class, ['attr' => ['class' => "p-2 my-2 bg-gray-200 rounded border-solid border-2 border-gray-400"]])
             ->add('description', TextType::class, ['attr' => ['class' => "p-2 my-2 bg-gray-200 rounded border-solid border-2 border-gray-400"]])
             ->add('contenu', TextType::class, ['attr' => ['class' => "p-2 my-2 bg-gray-200 rounded border-solid border-2 border-gray-400"]])
-            ->add('categorie', TextType::class, ['attr' => ['class' => "p-2 my-2 bg-gray-200 rounded border-solid border-2 border-gray-400"]])
+            ->add('categorie')
             ->add('Ajouter', SubmitType::class, array('attr' => array('class' => 'ml-6 p-2 px-5 bg-gray-200 rounded hover:bg-gray-400')))
         ;
     }
